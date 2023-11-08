9db527adaf3919cb51988041a3e123d3d948ea67
Joseph Dollahon <josephdollahon@gmail.com>
2023-11-08 10:35:59 -0800

    Refactor Group ID to Group Code

--- lib/church/create_group.py
+++ lib/church/create_group.py
@@ -1,12 +1,12 @@
 from firebase_admin import firestore
 
 
-def create_group(name, bio, image, is_private, journey_id, meeting_link, church_id, group_id):
+def create_group(name, bio, image, is_private, journey_id, meeting_link, church_id, group_code):
     db = firestore.client()
     church_ref = db.collection("churches").document(church_id)
     church = church_ref.get()
     church = church.to_dict()
     church_name = church["title"]
-    group = {"name": name, "bio": bio, "image": image, "is_private": is_private, "journey_id": journey_id, "meeting_link": meeting_link, "church_id": church_id, "church_name": church_name, "members": {}, "group_id": group_id, "is_active": True}
+    group = {"name": name, "bio": bio, "image": image, "is_private": is_private, "journey_id": journey_id, "meeting_link": meeting_link, "church_id": church_id, "church_name": church_name, "members": {}, "group_code": group_code, "is_active": True}
     update_time, group_ref = db.collection("groups").add(group)
     return group_ref
--- pages/create_group.py
+++ pages/create_group.py
@@ -17,7 +17,7 @@ with st.form("group_information", clear_on_submit=True):
     is_private = st.checkbox("Is group private")
     journey_id = st.text_input("Journey ID")
     meeting_link = st.text_input("Meeting Link")
-    group_id = st.text_input("Group ID")
+    group_code = st.text_input("Group Code")
 
     submitted = st.form_submit_button("Create Group")
 
@@ -26,5 +26,5 @@ if submitted:
     for church in churches:
         if church_selection == church["title"]:
             church_id = church["id"]
-    create_group(name, bio, image, is_private, journey_id, meeting_link, church_id, group_id)
+    create_group(name, bio, image, is_private, journey_id, meeting_link, church_id, group_code)
 
